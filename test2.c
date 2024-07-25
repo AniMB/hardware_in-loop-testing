114d4cb4c7b15da426679eadcc8ddb70933dcd9f
Animish <120055908+AniMB@users.noreply.github.com>
2024-07-25 15:43:13 -0700

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1 = 1, num2 = 20, sum;
+    int num1 = 1, num2 = 2, sum;
     sum = num1 + num2;
     printf("Sum of %d and %d is: %d\n", num1, num2, sum);
     return 0;
